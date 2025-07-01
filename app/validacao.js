bdf778b31ed0dca6606c8aae92015509e8901485
milenacasttro <milena.castro@sou.inteli.edu.br>
2025-07-01 20:27:12 -0300

    Refactor validation logic and enhance user feedback; update button styles for better UX

--- app/validacao.js
+++ app/validacao.js
@@ -3,17 +3,29 @@ function verificaSeOChutePossuiUmValorValido (chute){
     
     if (chuteForInvalido(numero)) {
         elementoChute.innerHTML += '<div>Valor Inválido</div>'
+        return;
     }
 
     if (numeroForMaiorOuMenorValorPermitido(numero)) {
         elementoChute.innerHTML +=
-        `<div>Valor Inválido: Fele um número entre ${menorValor} e ${maiorValor}</div>`
+        `<div>Valor Inválido: Fale um número entre ${menorValor} e ${maiorValor}</div>`
+        return;
     }
 
     if (numero === numeroSecreto) {
         document.body.innerHTML = `
             <h2>Você Acertou!!</h2>
             <h3>O número secreto era ${numeroSecreto}</h3>
+
+            <button id="jogar-novamente" class="btn-jogar">Jogar Novamente</button>
+        `
+    } else if (numero > numeroSecreto) {
+        elementoChute.innerHTML += `
+            <div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div>
+        `
+    } else {
+        elementoChute.innerHTML += `
+            <div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>
         `
     }
 }
@@ -25,3 +37,9 @@ function chuteForInvalido(numero) {
 function numeroForMaiorOuMenorValorPermitido(numero) {
     return numero > maiorValor || numero < menorValor
 }
+
+document.body.addEventListener('click', e => {
+    if (e.target.id == 'jogar-novamente') {
+        window.location.reload();
+    }
+})
